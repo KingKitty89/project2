1eb9179c83f95a6ff4f388ee0071dd2dca3d349d
Jessica Helft <jessicahelft@gmail.com>
2020-07-06 17:07:55 -0500

    working html routes

--- controller/drinkController.js
+++ controller/drinkController.js
@@ -14,88 +14,87 @@ router.get("/wine", (req, res) => {
   res.render("wine");
 });
 
+router.get("/api/bourbon/:aroma", (req, res) => {
+  db.Bourbon.findAll({
+    where: {
+      aroma: req.params.aroma
+    },
+    include: [
+      {
+        model: db.Aroma,
+        attributes: ["name"],
+        required: true
+      }
+    ]
+  })
+    .then(dbBourbon => {
+      res.json(dbBourbon);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/bourbon/:taste", (req, res) => {
+  db.Bourbon.findAll({
+    where: {
+      taste: req.params.taste
+    },
+    include: [
+      {
+        model: db.Taste,
+        attributes: ["name"],
+        required: true
+      }
+    ]
+  })
+    .then(dbBourbon => {
+      res.json(dbBourbon);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/wine/:aroma", (req, res) => {
+  db.Wine.findAll({
+    where: {
+      aroma: req.params.aroma
+    },
+    include: [
+      {
+        model: db.Aroma,
+        attributes: ["name"],
+        required: true
+      }
+    ]
+  })
+    .then(dbWine => {
+      res.json(dbWine);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/wine/:taste", (req, res) => {
+  db.Wine.findAll({
+    where: {
+      taste: req.params.taste
+    },
+    include: [
+      {
+        model: db.Tatse,
+        attributes: ["name"],
+        required: true
+      }
+    ]
+  })
+    .then(dbWine => {
+      res.json(dbWine);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 module.exports = router;
-
-// app.get("/api/bourbon/:aroma", (req, res) => {
-//   db.Bourbon.findAll({
-//     where: {
-//       aroma: req.params.aroma
-//     },
-//     include: [
-//       {
-//         model: db.Aroma,
-//         attributes: ["name"],
-//         required: true
-//       }
-//     ]
-//   })
-//     .then(dbBourbon => {
-//       res.json(dbBourbon);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/api/bourbon/:taste", (req, res) => {
-//   db.Bourbon.findAll({
-//     where: {
-//       taste: req.params.taste
-//     },
-//     include: [
-//       {
-//         model: db.Taste,
-//         attributes: ["name"],
-//         required: true
-//       }
-//     ]
-//   })
-//     .then(dbBourbon => {
-//       res.json(dbBourbon);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/api/wine/:aroma", (req, res) => {
-//   db.Wine.findAll({
-//     where: {
-//       aroma: req.params.aroma
-//     },
-//     include: [
-//       {
-//         model: db.Aroma,
-//         attributes: ["name"],
-//         required: true
-//       }
-//     ]
-//   })
-//     .then(dbWine => {
-//       res.json(dbWine);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/api/wine/:taste", (req, res) => {
-//   db.Wine.findAll({
-//     where: {
-//       taste: req.params.taste
-//     },
-//     include: [
-//       {
-//         model: db.Tatse,
-//         attributes: ["name"],
-//         required: true
-//       }
-//     ]
-//   })
-//     .then(dbWine => {
-//       res.json(dbWine);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
