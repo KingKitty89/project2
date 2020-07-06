ac57fb2eaeca16b015d1607968347d42ea7450cc
Jessica Helft <jessicahelft@gmail.com>
2020-07-06 16:42:16 -0500

    html routes working

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
 // Requiring necessary npm packages
 const express = require("express");
-const session = require("express-session");
-// Requiring passport as we've configured it
-const passport = require("./config/passport");
+const app = express();
 
-// Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
+
+const exphbs = require("express-handlebars");
+
+const routes = require("./controllers/drinkController.js");
 const db = require("./models");
 
-// Creating express app and configuring middleware needed for authentication
-const app = express();
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+app.use(routes);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
+db.sequelize.sync({}).then(() => {
   app.listen(PORT, () => {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser."
     );
   });
 });
