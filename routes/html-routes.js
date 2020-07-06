08fdb817abe88d168601c16bbbbfcdf97d7ea9f1
Jessica Helft <jessicahelft@gmail.com>
2020-07-06 13:30:47 -0500

    routes in progress

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,25 +1,40 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+const db = require("../models/index.js");
 //different pages for taste and aroma?
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    db.Drink.findAll(data => {
+      const hbsObject = {
+        drink: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
   });
 
-  app.get("/bourbon", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/bourbon.html"));
-  });
+  app.get("/bourbon", () => {
+    app.get("/", (req, res) => {
+      drink.all(data => {
+        const hbsObject = {
+          drink: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+      });
+    });
 
-  app.get("/wine", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/wine.html"));
-  });
+    app.get("/wine", (req, res) => {
+      res.sendFile(path.join(__dirname, "../public/wine.html"));
+    });
 
-  app.get("/bourbon-results", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/bourbon-results.html"));
-  });
+    app.get("/bourbon-results", (req, res) => {
+      res.sendFile(path.join(__dirname, "../public/bourbon-results.html"));
+    });
 
-  app.get("/wine-results", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/wine-results.html"));
+    app.get("/wine-results", (req, res) => {
+      res.sendFile(path.join(__dirname, "../public/wine-results.html"));
+    });
   });
 };
