08f2f964eb27f674d3e4eeadec4263017bdce85d
Jessica Helft <jessicahelft@gmail.com>
2020-07-07 13:51:02 -0500

    trying

--- public/js/wine.js
+++ public/js/wine.js
@@ -19,36 +19,20 @@ $(document).ready(() => {
     if (!userInput.tasteInput || !userInput.aromaInput) {
       return;
     }
-    // getNotes(userInput.tasteInput, userInput.aromaInput);
-  });
-  let tasteInput = taste.val().trim();
-
-  $.get("/api/wine/:taste", + tasteInput, function(data){
-    console.log(taste.val());
-    if (data.length !== 0) {
-      for (var i = 0; i < data.length; i++) {
-      } else {
-        $('#put-cards-here').addClass('.no-games').text('No games found!');
-      }
-    });
-
-    taste.val("");
-
+    getNotes(userInput.tasteInput, userInput.aromaInput);
   });
 
-  
-
-  // function getNotes(taste, aroma) {
-  //   $.get("/api/wine/:aroma", {
-  //     aromaInput: aroma
-  //     tasteInput: taste
-  //   })
-  //     .then(() => {
-  //       res.redirect("/wine-results");
-  //       // If there's an error, log the error
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }
+  function getNotes(taste, aroma) {
+    $.get("/api/wine/:aroma", {
+      aroma: aroma,
+      tasteInput: taste
+    })
+      .then(() => {
+        res.redirect("/wine-results");
+        // If there's an error, log the error
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 });
